04b3cf1f9e8cdcfbc779bfc83ad33fd45acc7211
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2024-05-02 03:02:31 +0700

    upload driver_fe

---
+++ driver_fe/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:driver_fe/my_home_page.dart';
+import 'package:flutter/material.dart';
+import 'login.dart'; 
+import 'bottom_navigation_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 71, 169, 146)),
+        useMaterial3: true,
+      ),
+      home: LoginScreen(), 
+    );
+  }
+}
